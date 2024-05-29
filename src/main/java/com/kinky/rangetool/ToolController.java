65b2ebfc419ec01b0685e2b4cd2c2790d91ea549
KinkyJalepeno <wolfhammer@live.co.uk>
2024-05-29 11:40:21 +0100

    Added file chooser and valid path logic only thing left now is to implement the file reader and write logic

--- src/main/java/com/kinky/rangetool/ToolController.java
+++ src/main/java/com/kinky/rangetool/ToolController.java
@@ -2,24 +2,46 @@ package com.kinky.rangetool;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.io.File;
 
 public class ToolController {
+
+    private Window Stage;
+    String pathToFile;
+    boolean isPathValid = false;
 
     @FXML
     private TextField status;
 
 
+
     @FXML
     void browseToFile() {
 
-        status.setText("You pressed browse to file");
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(Stage);
 
+        pathToFile = file.getAbsolutePath();
+        status.setText(pathToFile);
+        isPathValid = true;
     }
+
+
 
     @FXML
     void processFile() {
 
-        status.setText("You pressed process file");
+        if (!isPathValid){
+
+            status.setText("Select a file first !");
+
+        }else{
+
+            status.setText("You pressed process");
+        }
 
     }
 
